e26e930cf2f2b959e9b73007fab7d8d79a6dee63
Olabamipe Taiwo <teeola48@gmail.com>
2020-02-26 13:02:14 +0000

    Side Links animation

---
+++ Object.values(activeIntroLink).forE.js
@@ -0,0 +1,13 @@
+ // Object.values(activeIntroLink).forEach((link,index) => {
+    //     if(link.classList.contains("notice")) {
+    //      link.classList.remove("notice");
+    //      link.parentElement.parentElement
+    //                           .nextElementSibling
+    //                           .childNodes[3].childNodes[1]
+    //                           .className = "introduction__links__item-icon-circle notice";
+    //         // console.log(link.parentElement.parentElement.nextElementSibling.childNodes[3].childNodes[1].classList);
+    //     }else {
+
+    //     }
+       
+    // });
--- js/main.js
+++ js/main.js
@@ -1,19 +1,40 @@
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const signIn = document.querySelector('.signin');
-const signUpOverlay = document.querySelector('.overlay__signup');
-const signInOverlay= document.querySelector('.overlay__signin');
-const signInContainer = document.querySelector('.sign-in-container');
+var index = 0; 
+
+// const activeIntroLink = document.querySelector(".introduction__links__item a");
+// activeIntroLink.parentElement
+//                 .nextElementSibling
+//                 .childNodes[1]
+//                 .className = "notice";
 
 
-signUpButton.addEventListener('click',  () => {
-    signIn.classList.add('active');
-    signInContainer.classList.add('active');
-    signUpOverlay.classList.add('active');
-});
 
-signInButton.addEventListener('click',  () => {
-    signIn.classList.remove('active');
-    signInContainer.classList.remove('active');
-    signUpOverlay.classList.remove('active');
-});
+const notice = () => {   
+  const activeIntroLink = document.getElementsByClassName("introduction__links__item-link")[index]; 
+  const activeIntroIcon = document.getElementsByClassName("introduction__links__item-icon-circle")[index];
+
+
+  if(activeIntroIcon.parentElement.parentElement.nextElementSibling) {
+      activeIntroLink.classList.remove("notice");
+      activeIntroIcon.classList.remove("notice");
+
+      activeIntroLink.parentElement
+                  .nextElementSibling
+                  .childNodes[1]
+                  .className = "introduction__links__item-link notice";
+      activeIntroIcon.parentElement.parentElement
+                  .nextElementSibling
+                  .childNodes[3].childNodes[1]
+                  .className = "introduction__links__item-icon-circle notice";
+       index++;
+  }else {
+      activeIntroIcon.classList.remove("notice");
+      activeIntroLink.classList.remove("notice");
+      index = 0;
+      document.getElementsByClassName("introduction__links__item-link")[0].className = "introduction__links__item-link notice";
+      document.getElementsByClassName("introduction__links__item-icon-circle")[0].className = "introduction__links__item-icon-circle notice";
+    }
+} 
+
+
+setInterval(notice, 1000);
+
